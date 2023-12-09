e6098fd24a576a774a10c234a7f6b8b136d0f04b
Frederico Cabral <fredpcabral@gmail.com>
2023-12-09 22:09:22 +0100

    refactoring

--- 2023/day-01/go/main_test.go
+++ 2023/day-01/go/main_test.go
@@ -9,7 +9,7 @@ func TestCalibration(t *testing.T) {
 		calibration := calibrate("1abc2")
 
 		// assert
-		assertCalibration(t, calibration, 12)
+		assert(t, calibration, 12)
 	})
 
 	t.Run("in the value", func(t *testing.T) {
@@ -17,7 +17,7 @@ func TestCalibration(t *testing.T) {
 		calibration := calibrate("pqr3stu8vwx")
 
 		// assert
-		assertCalibration(t, calibration, 38)
+		assert(t, calibration, 38)
 	})
 
 	t.Run("when there are more than two digits", func(t *testing.T) {
@@ -25,14 +25,14 @@ func TestCalibration(t *testing.T) {
 		calibration := calibrate("a1b2c3d4e5f")
 
 		// assert
-		assertCalibration(t, calibration, 15)
+		assert(t, calibration, 15)
 	})
 
 	t.Run("when there is only one digit", func(t *testing.T) {
 		// act
 		calibration := calibrate("treb7uchet")
 		// assert
-		assertCalibration(t, calibration, 77)
+		assert(t, calibration, 77)
 	})
 }
 
@@ -46,16 +46,10 @@ func TestSumOfAllCalibrationValues(t *testing.T) {
 	got := sumAll(calibrations)
 
 	// assert
-	assertSum(t, got, 50)
+	assert(t, got, 50)
 }
 
-func assertCalibration(t *testing.T, received, expected int) {
-	if received != expected {
-		t.Fatalf("received (%d) but expected (%d)", received, expected)
-	}
-}
-
-func assertSum(t *testing.T, received, expected int) {
+func assert(t *testing.T, received, expected int) {
 	if received != expected {
 		t.Fatalf("received (%d) but expected (%d)", received, expected)
 	}
