d02d3a6a32bb9a3e0ac67de2a95d9794972be8b1
Frederico Cabral <fredpcabral@gmail.com>
2023-12-13 16:58:16 +0100

    Parsing input line

--- 2023/day-02/main.go
+++ 2023/day-02/main.go
@@ -1,7 +1,53 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	GREEN = "green"
+	BLUE  = "blue"
+	RED   = "red"
+)
+
 func main() {
 
+	readInputFromFile("input.txt")
+
+}
+
+func ParseInput(lines []string) [][]map[string]int {
+	var games [][]map[string]int
+
+	for _, line := range lines {
+		game := strings.Split(line, ":")[1]
+		sets := strings.Split(game, ";")
+
+		var setsToAdd []map[string]int
+		for _, set := range sets {
+
+			cubesRevealed := strings.Split(set, ",")
+
+			cubeToAdd := make(map[string]int)
+			for _, cube := range cubesRevealed {
+				cubeSliptted := strings.Split(cube, " ")
+				color := cubeSliptted[2]
+				quantity, _ := strconv.Atoi(cubeSliptted[1])
+				cubeToAdd[color] = quantity
+			}
+
+			setsToAdd = append(setsToAdd, cubeToAdd)
+
+		}
+
+		games = append(games, setsToAdd)
+	}
+
+	return games
 }
 
 func SumNumberOfCuber(game []map[string]int) map[string]int {
@@ -35,4 +81,35 @@ func IsItPossible(game map[string]int) bool {
 	}
 
 	return true
+}
+
+func SumAllPossibleGames(gamesResult []bool) int {
+
+	sum := 0
+
+	for i, isPossible := range gamesResult {
+		if isPossible {
+			sum = sum + i + 1
+		}
+	}
+
+	return sum
+}
+
+func readInputFromFile(fileName string) []string {
+	var input []string
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+
+	return input
 }
--- 2023/day-02/main_test.go
+++ 2023/day-02/main_test.go
@@ -4,12 +4,6 @@ import (
 	"testing"
 )
 
-const (
-	GREEN = "green"
-	BLUE  = "blue"
-	RED   = "red"
-)
-
 func TestSumNumberOfCubes(t *testing.T) {
 
 	game := []map[string]int{
@@ -40,55 +34,109 @@ func TestSumNumberOfCubes(t *testing.T) {
 func TestIsPossible(t *testing.T) {
 
 	t.Run("No, if red above limit", func(t *testing.T) {
-
 		sumOfCubes := map[string]int{GREEN: 26, BLUE: 11, RED: 25}
 
-		got := IsItPossible(sumOfCubes)
-		expected := false
-
-		if got != expected {
-			t.Fatalf("got (%t) bue expected (%t)", got, expected)
+		if IsItPossible(sumOfCubes) {
+			t.Fatalf("this game should not be possible")
 		}
 
 	})
 
 	t.Run("No, if green above limit", func(t *testing.T) {
-
 		sumOfCubes := map[string]int{GREEN: 14, BLUE: 5, RED: 10}
 
-		got := IsItPossible(sumOfCubes)
-		expected := false
-
-		if got != expected {
-			t.Fatalf("got (%t) bue expected (%t)", got, expected)
+		if IsItPossible(sumOfCubes) {
+			t.Fatalf("this game should not be possible")
 		}
 
 	})
 
 	t.Run("No, if blue above limit", func(t *testing.T) {
-
 		sumOfCubes := map[string]int{GREEN: 11, BLUE: 15, RED: 10}
 
-		got := IsItPossible(sumOfCubes)
-		expected := false
-
-		if got != expected {
-			t.Fatalf("got (%t) bue expected (%t)", got, expected)
+		if IsItPossible(sumOfCubes) {
+			t.Fatalf("this game should not be possible")
 		}
 
 	})
 
 	t.Run("Yes, if all below limit", func(t *testing.T) {
-
 		sumOfCubes := map[string]int{GREEN: 13, BLUE: 14, RED: 12}
 
-		got := IsItPossible(sumOfCubes)
-		expected := true
-
-		if got != expected {
-			t.Fatalf("got (%t) bue expected (%t)", got, expected)
+		if !IsItPossible(sumOfCubes) {
+			t.Fatalf("this game should be possible")
 		}
 
 	})
 
+}
+
+func TestSumAllPossibleGames(t *testing.T) {
+
+	games := []bool{true, true, false, false, true}
+
+	got := SumAllPossibleGames(games)
+	expected := 8
+
+	if expected != got {
+		t.Fatalf("expected %d, but got %d", expected, got)
+	}
+
+}
+
+func TestParseInput(t *testing.T) {
+
+	lines := []string{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red",
+	}
+
+	received := ParseInput(lines)
+
+	expected := [][]map[string]int{
+		{
+			{BLUE: 3, RED: 4},
+			{GREEN: 2, BLUE: 6, RED: 1},
+			{GREEN: 2},
+		},
+		{
+			{BLUE: 1, GREEN: 2},
+			{GREEN: 3, BLUE: 4, RED: 1},
+		},
+	}
+
+	if len(received) != len(expected) {
+		t.Fatalf("Sizes don't match")
+	}
+
+	for i := 0; i < len(received); i++ {
+
+		if len(received[i]) != len(expected[i]) {
+			t.Fatalf("erro \n received: %d \n expected: %d", len(received[i]), len(expected[i]))
+		}
+
+		for j := 0; j < len(received[i]); j++ {
+
+			if !mapsAreEqual(received[i][j], expected[i][j]) {
+				t.Fatalf("not equal \n Received:%v \n Expected:%v", received[i][j], expected[i][j])
+			}
+
+		}
+
+	}
+}
+
+func mapsAreEqual(map1, map2 map[string]int) bool {
+	if len(map1) != len(map2) {
+		return false
+	}
+
+	for key, value1 := range map1 {
+		value2, ok := map2[key]
+		if !ok || value1 != value2 {
+			return false
+		}
+	}
+
+	return true
 }
